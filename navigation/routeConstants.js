375aa6be5ce2b72260b40a17f417ec8826df407d
hsarpwintoo <6731503074@lamduan.mfu.ac.th>
2026-02-12 18:41:27 +0700

    Add route constants for navigation

---
+++ navigation/routeConstants.js
@@ -0,0 +1,17 @@
+/**
+ * Route Constants
+ * Centralized constants for all navigation routes
+ */
+
+export const ROUTES = {
+  // Auth Stack
+  LOGIN: 'Login',
+  
+  // App Stack
+  HOME: 'Home',
+};
+
+export const STACKS = {
+  AUTH: 'AuthStack',
+  APP: 'AppStack',
+};
