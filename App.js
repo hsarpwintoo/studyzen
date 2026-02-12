180a84ea8ae4b1b909725fcc1e51b28ab34f775f
hsarpwintoo <6731503074@lamduan.mfu.ac.th>
2026-02-12 18:42:43 +0700

    Create App.js for the main application entry point.

---
+++ App.js
@@ -0,0 +1,13 @@
+/**
+ * App Entry Point
+ * Root component of the application
+ */
+
+import React from 'react';
+import RootNavigator from './navigation/RootNavigator';
+
+const App = () => {
+  return <RootNavigator />;
+};
+
+export default App;
