ccacb9beec043deee2ff96eea43f90b5f897b95a
hsarpwintoo <6731503074@lamduan.mfu.ac.th>
2026-02-13 14:29:17 +0700

    Create FocusTimerScreen.js

---
+++ features/focusTimer/screens/FocusTimerScreen.js
@@ -0,0 +1,13 @@
+// FocusTimerScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FocusTimerScreen = () => {
+    return (
+        <View>
+            <Text>Focus Timer Screen</Text>
+        </View>
+    );
+};
+
+export default FocusTimerScreen;
